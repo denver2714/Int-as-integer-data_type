fe5c33f91a8d6f3010eb5df1c00951b4d92ec757
denverplus <118152975+denver2714@users.noreply.github.com>
2024-03-12 17:18:54 +0800

    Update Int.h

--- Int/Int.h
+++ Int/Int.h
@@ -9,8 +9,8 @@ class Int {
 private:
     int value;
 public:
-
-    Int(int rhs = 1);
+    Int();
+    Int(int rhs);
     friend Int operator+(const Int& number1, const Int& number2);
     friend std::ostream& operator<<(std::ostream& os,const Int& out);
     friend std::istream& operator>>(std::istream& is, Int& in);
