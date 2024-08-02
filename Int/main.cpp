fc1d4e14c051c2cb0f068689babb77c1e59198c9
Kurt Chavez <kurtdenverchavez2005@gmail.com>
2024-08-03 01:47:06 +0800

    changed

--- Int/main.cpp
+++ Int/main.cpp
@@ -3,6 +3,18 @@
 
 int main() {
 
-    std::cout << "I am going to delete this branch soon, if i dont , im dead" << std::endl; 
+    Int num1 = 10;
+    Int num2 = 20;
+    Int num3 = num1 + num2;
+    Int user_number;
+
+    std::cout << "Initial number: " << num3 << std::endl;
+    std::cout << "Enter the number to add to the initial number: ";
+    std::cin >> user_number;
+    num3 += user_number;
+
+    std::cout << "Final number: " << num3 << std::endl;
+
+
     return 0;
 }
