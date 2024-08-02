80989a2368191acfda03ddb863eeffdd289f28ef
Kurt Chavez <kurtdenverchavez2005@gmail.com>
2024-08-03 01:44:34 +0800

    Might delete later type shit, this repo

--- Int/main.cpp
+++ Int/main.cpp
@@ -3,18 +3,6 @@
 
 int main() {
 
-    Int num1 = 10;
-    Int num2 = 20;
-    Int num3 = num1 + num2;
-    Int user_number;
-
-    std::cout << "Initial number: " << num3 << std::endl;
-    std::cout << "Enter the number to add to the initial number: ";
-    std::cin >> user_number;
-    num3 += user_number;
-
-    std::cout << "Final number: " << num3 << std::endl;
-
-
+    std::cout << "I am going to delete this branch soon, if i dont , im dead" << std::endl; 
     return 0;
 }
